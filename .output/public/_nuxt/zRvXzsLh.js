cf94515406bd689c0659f09f433dc0f42b95469d
ricoaldy <ricoaldyy@gmail.com>
2026-01-13 14:01:13 +0700

    output

--- .output/public/_nuxt/zRvXzsLh.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./DlAUqK2U.js";import{b as o,o as s,a0 as t}from"./Dy6-hulR.js";const n={};function a(e,c){return s(),o("tr",null,[t(e.$slots,"default")])}const f=Object.assign(r(n,[["render",a]]),{__name:"ProseTr"});export{f as default};
